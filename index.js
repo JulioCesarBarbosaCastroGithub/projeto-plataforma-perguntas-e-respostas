6c9eb9b677d110e8b29d54e3ab8d250c2ff3d204
JulioCesarBarbosaCastroGithub <juliocesarbarbosacastro@outlook.com>
2022-09-22 20:30:15 -0300

    fourth commit

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 app.set('view engine', 'ejs');
+app.use(express.text());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
 app.get('/perguntar', (req, res ) => {
-    res.render('perguntar');
+  res.render('perguntar');
+});
+
+app.post('/salvarpergunta', (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+  res.send("Formulário recebido! Título: " + titulo + " " + "Descrição: " + descricao);
 });
 
 app.listen(8080, () => {
-    console.log("Servidor rodando...");
+  console.log("Servidor rodando...");
 });
 
